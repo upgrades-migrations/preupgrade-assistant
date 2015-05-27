fc5a760cb1820c5b2770273715a80c1fefeaed28
Alois Mahdal <amahdal@redhat.com>
2015-05-27 04:52:11 +0200

    Port assertIsNone and assertIsNotNone from 2.7

--- tests/base.py
+++ tests/base.py
@@ -8,4 +8,18 @@ for unittest.TestCase.
 
 
 class TestCase(unittest.TestCase):
-    pass
+
+    # ported from 2.7's unittest
+    def assertIsNone(self, obj, msg=None):
+        """Same as self.assertTrue(obj is None), with a nicer
+        default message."""
+        if obj is not None:
+            standardMsg = '%r is not None' % obj
+            self.fail(self._formatMessage(msg, standardMsg))
+
+    # ported from 2.7's unittest
+    def assertIsNotNone(self, obj, msg=None):
+        """Included for symmetry with assertIsNone."""
+        if obj is None:
+            standardMsg = 'unexpectedly None'
+            self.fail(self._formatMessage(msg, standardMsg))
