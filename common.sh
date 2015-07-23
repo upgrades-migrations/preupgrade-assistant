5bb6ff5b17c446d696e8d3950e939b7a20253465
Petr Stodulka <pstodulk@redhat.com>
2015-07-23 15:41:39 +0200

    Added functions for parsing of PA config files to common.sh removed bashism from common.sh script

--- common.sh
+++ common.sh
@@ -1,6 +1,7 @@
 
 CACHE=/var/cache/preupgrade
 PREUPGRADE_CACHE=/var/cache/preupgrade/common
+PREUPGRADE_CONFIG=/etc/preupgrade-assistant.conf
 VALUE_RPM_QA=$PREUPGRADE_CACHE/rpm_qa.log
 VALUE_ALL_CHANGED=$PREUPGRADE_CACHE/rpm_Va.log
 VALUE_CONFIGCHANGED=$PREUPGRADE_CACHE/rpm_etc_Va.log
@@ -28,6 +29,10 @@ RESULT_NOT_APPLICABLE=$XCCDF_RESULT_NOT_APPLICABLE
 RESULT_FIXED=$XCCDF_RESULT_FIXED
 RESULT_INFORMATIONAL=$XCCDF_RESULT_INFORMATIONAL
 
+# variables set by PA config file #
+HOME_DIRECTORY_FILE=""
+USER_CONFIG_FILE=0
+
 export LC_ALL=C
 
 # general logging function
@@ -41,7 +46,7 @@ export LC_ALL=C
 #
 # Note that if env variable $COMPONENT is defined, it may be omitted from
 # parameters.
-function log
+log()
 {
     SEVERITY=$1 ; shift
     if test -z "$COMPONENT"; then
@@ -60,97 +65,97 @@ function log
     echo "$SEVERITY $COMPONENT: $1"
 }
 
-function log_debug
+log_debug()
 {
     log "DEBUG" "$@"
 }
 
-function log_info
+log_info()
 {
     log "INFO" "$@"
 }
 
-function log_error
+log_error()
 {
     log "ERROR" "$@"
 }
 
-function log_warning
+log_warning()
 {
     log "WARNING" "$@"
 }
 
-function log_risk
+log_risk()
 {
     echo "INPLACERISK: $1: $2" >&2
 }
 
-function log_none_risk
+log_none_risk()
 {
     log_risk "NONE" "$1"
 }
 
-function log_slight_risk
+log_slight_risk()
 {
     log_risk "SLIGHT" "$1"
 }
 
-function log_medium_risk
+log_medium_risk()
 {
     log_risk "MEDIUM" "$1"
 }
 
-function log_high_risk
+log_high_risk()
 {
     log_risk "HIGH" "$1"
 }
 
-function log_extreme_risk
+log_extreme_risk()
 {
     log_risk "EXTREME" "$1"
 }
 
-function exit_unknown
+exit_unknown()
 {
     exit $RESULT_UNKNOWN
 }
 
-function exit_pass
+exit_pass()
 {
     exit $RESULT_PASS
 }
 
-function exit_fail
+exit_fail()
 {
     exit $RESULT_FAIL
 }
 
-function exit_error
+exit_error()
 {
     exit $RESULT_ERROR
 }
 
-function exit_not_applicable
+exit_not_applicable()
 {
     exit $RESULT_NOT_APPLICABLE
 }
 
-function exit_informational
+exit_informational()
 {
     exit $RESULT_INFORMATIONAL
 }
 
-function exit_fixed
+exit_fixed()
 {
     exit $RESULT_FIXED
 }
 
-function switch_to_content
+switch_to_content()
 {
     cd $CURRENT_DIRECTORY
 }
 
-function check_applies_to
+check_applies_to()
 {
     local RPM=1
     if [ -z "$1" ]
@@ -177,7 +182,7 @@ function check_applies_to
     fi
 }
 
-function check_rpm_to
+check_rpm_to()
 {
     local RPM=1
     local BINARY=1
@@ -227,7 +232,7 @@ function check_rpm_to
     fi
 }
 # This check can be used if you need root privilegues
-function check_root
+check_root()
 {
     if [ "$(id -u)" != "0" ]; then
         log_error "This script must be run as root"
@@ -236,21 +241,19 @@ function check_root
     fi
 }
 
-function solution_file
+solution_file()
 {
     echo "$1" >> $SOLUTION_FILE
 }
 
 
 # returns true if service in $1 is enabled in any runlevel
-function service_is_enabled {
+service_is_enabled() {
     if [ $# -ne 1 ] ; then
         echo "Usage: service_is_enabled servicename"
         return 2
     fi
-    if grep -e "^${1}.*:on" "$VALUE_CHKCONFIG" &>/dev/null ; then
-        return 0
-    fi
+    grep -qe "^${1}.*:on" "$VALUE_CHKCONFIG" && return 0
     return 1
 }
 
@@ -258,22 +261,109 @@ function service_is_enabled {
 # true if cp succeeds,
 # 1 if config file doesn't exist
 # 2 if config file was not changed and thus is not necessary to back-up
-function backup_config_file {
+backup_config_file() {
     CONFIG_FILE=$1
 
     # config file exists?
-    if [ ! -f $CONFIG_FILE ] ; then
+    if [ ! -f "$CONFIG_FILE" ] ; then
         return 1
     fi
 
     # config file is changed?
-    if ! grep -e " ${CONFIG_FILE}" ${VALUE_CONFIGCHANGED} >/dev/null; then
-        return 2
-    fi
+    grep -qe " ${CONFIG_FILE}" ${VALUE_CONFIGCHANGED} || return 2
 
-    mkdir -p "${VALUE_TMP_PREUPGRADE}/$(dirname $CONFIG_FILE)"
+    mkdir -p "${VALUE_TMP_PREUPGRADE}/$(dirname "$CONFIG_FILE")"
     cp -f "${CONFIG_FILE}" "${VALUE_TMP_PREUPGRADE}${CONFIG_FILE}"
     return $?
 }
 
+space_trim() {
+  echo "$@" | sed -r "s/^\s*(.*)\s*$/\1/"
+}
+
+# functions for easy parsing of config files
+# returns 0 on success, otherwise 1
+# requires path
+conf_get_sections() {
+  [ $# -eq 1 ] || return 1
+  [ -f "$1" ] || return 1
+
+  grep -E "^\[.+\]$" "$1" | sed -r "s/^\[(.+)\]$/\1/"
+  return $?
+}
+
+# get all items from config file $1 inside section $2
+# e.g.: conf_get_section CONFIG_FILE section-without-brackets
+conf_get_section() {
+  [ $# -eq 2 ] || return 1
+  [ -f "$1" ] || return 1
+  _section=""
+  while read line; do
+    [ -z "$line" ] && continue
+    echo "$line" | grep -q "^\[..*\]$" && {
+      _section="$(echo "$line" | sed -E "s/^\[(.+)\]$/\1/")"
+      continue # that's new section
+    }
+    [ -z "$_section" ] && continue
+
+    #TODO: do not print comment lines?
+    [ "$_section" == "$2" ] && echo "$line" |grep -vq "^#.*$" && echo "$line"
+  done < "$1"
+
+  return 0
+}
+
+
+# here is parsed PA configuration
+load_pa_configuration() {
+  # this is main function for parsing
+  [ -f "$PREUPGRADE_CONFIG" ] && [ -r "$PREUPGRADE_CONFIG" ] || {
+    log_error "Configuration file $PREUPGRADE_CONFIG is missing or is not readable!"
+    exit_error
+  }
+  _pa_conf="$(conf_get_section "$PREUPGRADE_CONFIG" "preupgrade-assistant")"
+  [ -z "$_pa_conf" ] && {
+    log_error "Can't load any configuration from section preupgrade-assistant!"
+    exit_error
+  }
+  #TODO: what about empty _pa_conf??
+  #TODO: enable comments only when # is first character on line
+  #TODO: files which begins witch # or looks like sections, should start
+  #      with e.g. "./" prefix, which kept same directory - or solve it
+  #      by some function here too??
+  printf "%s\n" "$_pa_conf" | while read line; do
+    tmp_option=$(space_trim "$(echo "$line" | cut -d "=" -f 1)")
+    tmp_val=$(space_trim "$(echo "$line" | cut -d "=" -f 2-)")
+    echo "$line -- $tmp_option - $tmp_val -"
+    # HERE add your actions
+    case $tmp_option in
+      home_directory_file)
+        HOME_DIRECTORY_FILE="$tmp_val"
+        ;;
+      user_config_file)
+        USER_CONFIG_FILE=$([ "$tmp_val" == "enabled" ] && echo 1 || echo 0)
+        ;;
+      *) log_error "Unknown option $tmp_option"; exit_error
+    esac
+  done
+}
+
+# print items from [home-dirs] which are relevant for given user
+# when username is not given or config file for user is not enabled,
+# items from main configuration file is printed
+# returns 0 on SUCCESS, otherwise 1 and logs warning
+# shouldn't be used before load_config_parser
+print_home_dirs() {
+  [ $# -eq 1 ] && [ $USER_CONFIG_FILE -eq 1 ] || {
+    conf_get_section "$PREUPGRADE_CONFIG" "home-dirs"
+    return 0
+  }
+
+  _uconf_file="/home/$1/$HOME_DIRECTORY_FILE"
+  [ -f "$_uconf_file" ] || return 0 # missing file in user's home dir is OK
+  conf_get_section "$_uconf_file" "home-dirs"
+}
+
+
+load_pa_configuration
 switch_to_content
