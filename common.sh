228c64cd4bcf477f5bd1f668065541872ac0aee3
Petr Stodulka <pstodulk@redhat.com>
2016-02-15 16:16:47 +0100

    common.sh: fix get_dist_native_list
    - use $VALUE_RPM_QA instead of $VALUE_RPM_RHSIGNED to get
      list of really all native packages

--- common.sh
+++ common.sh
@@ -373,7 +373,7 @@ get_dist_native_list() {
   while read line; do
     pkg=$(echo $line | cut -d " " -f1 )
     is_dist_native $pkg >/dev/null && echo $pkg
-  done < "$VALUE_RPM_RHSIGNED"
+  done < "$VALUE_RPM_QA"
 }
 
 # here is parsed PA configuration
