9f8efd90dde24200632f002fcd44214ddef3374d
Petr Hracek <phracek@redhat.com>
2016-09-21 10:08:55 +0200

    Typo in variable name
    Signed-off-by: Petr Hracek <phracek@redhat.com>

--- tests/test_api.py
+++ tests/test_api.py
@@ -130,13 +130,13 @@ class TestAPICheck(base.TestCase):
             shutil.rmtree(hook_dir)
         deploy_type = "preupgrade"
         script_api.deploy_hook(deploy_type, "setup.py", "common.sh", "/usr/bin/cp")
-        postupgrade_hook_dir = os.path.join(script_api.VALUE_TMP_PREUPGRADE,
+        preupgrade_hook_dir = os.path.join(script_api.VALUE_TMP_PREUPGRADE,
                                             "hooks",
                                             "xccdf" + script_api.MODULE_PATH,
                                             deploy_type,
                                             )
         for f in ["run_hook", "common.sh", "cp"]:
-            self.assertTrue(os.path.isfile(os.path.join(postupgrade_hook_dir, f)))
+            self.assertTrue(os.path.isfile(os.path.join(preupgrade_hook_dir, f)))
 
 
 def suite():
