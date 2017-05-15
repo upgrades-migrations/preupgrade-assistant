b8e8e39887a45a8cb32151bf834485e77b7cd690
Michal Bocek <mbocek@redhat.com>
2017-05-15 22:53:53 +0200

    Add basic example of writing unit tests
    - propose unit test conventions
    - update preupg_diff tests to match the conventions

--- tests/test_preupg_diff.py
+++ tests/test_preupg_diff.py
@@ -4,12 +4,19 @@ try:
     import base
 except ImportError:
     import tests.base as base
+import inspect
+import os
 import sys
 
 from preupg import preupg_diff
 
 
 class TestPreupgDiff(base.TestCase):
+
+    current_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
+
+    class argv_mocked(list):
+        pass
 
     class dummy_mock(base.MockFunction):
         def __call__(self, *args, **kwargs):
@@ -20,19 +27,24 @@ class TestPreupgDiff(base.TestCase):
             self.num_new_rules = num_new_xml_rules
             self.num_diff_rules = num_diff_xml_rules
 
-    @base.mock(sys, "argv", [
-        "preupg-diff",
-        "tests/generated_results/inplace_combined_risk_test.xml",
-        "tests/generated_results/inplace_risk_test.xml"])
+    @base.mock(sys, "argv", argv_mocked)
     @base.mock(preupg_diff, "save_diff_to_xml_and_html_file", dummy_mock())
     @base.mock(preupg_diff, "print_difference_status",
                print_difference_status_mock())
     def test_xml_on_input(self):
+        sys.argv = [
+            "preupg-diff",
+            os.path.join(self.current_dir,
+                         "generated_results",
+                         "inplace_combined_risk_test.xml"),
+            os.path.join(self.current_dir,
+                         "generated_results",
+                         "inplace_risk_test.xml")]
+
         preupg_diff.run()
 
         self.assertEqual(preupg_diff.print_difference_status.num_new_rules, 2)
         self.assertEqual(preupg_diff.print_difference_status.num_diff_rules, 1)
-
 
     class stringify_children_mock(base.MockFunction):
         def __call__(self, tag_obj):
@@ -41,7 +53,9 @@ class TestPreupgDiff(base.TestCase):
 
     @base.mock(preupg_diff, "stringify_children", stringify_children_mock())
     def test_stringify_solution_text(self):
-        preupg_diff.ResultXML("tests/generated_results/inplace_risk_test.xml")
+        preupg_diff.ResultXML(os.path.join(self.current_dir,
+                                           "generated_results",
+                                           "inplace_risk_test.xml"))
         self.assertEqual(preupg_diff.stringify_children.children_as_str,
                          'str1<br xmlns:xhtml="http://www.w3.org/1999/xhtml/"'
                          ' xmlns:ns0="http://checklists.nist.gov/xccdf/1.2"/>'
