4f423c365287868eb13e0443b894d46f09abb90d
Majo Durco <majodurco@gmail.com>
2017-08-03 15:06:38 +0200

    Fix string format and typo

--- preupg/creator/settings.py
+++ preupg/creator/settings.py
@@ -37,10 +37,10 @@ summary_title = 'preupg-content-creator generated these files to be updated:'
 summary_directory = '- the module was created in the %s directory.'
 summary_ini = '- the INI file which defines the module is %s.'
 summary_check = '- the check script which provides an assessment is %s. Update it before you use it.'
-summary_solution = '- the solution text which informs about incompatilibies is %s. Update it before you use it.'
+summary_solution = '- the solution text which informs about incompatibilities %s. Update it before you use it.'
 check_path = "The %s file already exists. Do you want to replace the file?"
 type_check_script = "Would you like to create a BASH or Python check script? [sh/py] Bash is default."
-properties_ini = "Fill empty options in %s before you use it."
+properties_ini = "- Fill empty options in %s before you use it."
 
 text_for_testing = "\nFor testing content run these two commands:\n" \
                    "- preupg-create-group-xml %s to create XML file\n" \
